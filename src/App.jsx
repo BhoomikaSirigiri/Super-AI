ac8bd2081ef1b279c6c78ce42e8c71398cf0c6ef
Bhoomika Sirigiri <bhumikabd98@gmail.com>
2026-02-02 23:07:02 +0530

    Fix logo.png case sensitivity for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import logo from './assets/logo.png'
+import logo from './assets/Logo.png'
 import chat from './assets/chat.png'
 import video from './assets/video.png'
 import image from './assets/image.png'
